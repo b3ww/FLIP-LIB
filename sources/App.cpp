a83bb2b614c59d9f6db529d823970e9d52f65ef1
Popochounet <adrien.audiard@epitech.eu>
2024-05-02 17:14:12 +0200

    [UPD] App.cpp: use of Socket class

--- sources/App.cpp
+++ sources/App.cpp
@@ -16,34 +16,13 @@
 
 namespace flip {
     App::App(uint16_t port, const std::string &name, const __FLIP_routeMap &routesMap) :
-    _name(name), _routesMap(routesMap)
+    _name(name), _routesMap(routesMap), _serverSocket(port)
     {
         sem_init(&requests, 1, 0);
-        _serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-        if (_serverSocket < 0) {
-            exit(EXIT_FAILURE);
-        }
-
-        sockaddr_in serverAddr;
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_addr.s_addr = INADDR_ANY;
-        serverAddr.sin_port = htons(port);
-
-        if (bind(_serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-            close(_serverSocket);
-            exit(EXIT_FAILURE);
-        }
-
-        if (listen(_serverSocket, SOMAXCONN) < 0) {
-            close(_serverSocket);
-            exit(EXIT_FAILURE);
-        }
     }
 
     App::~App()
     {
-        if(_serverSocket != -1)
-            close(_serverSocket);
     }
 
     void App::run()
@@ -52,18 +31,12 @@ namespace flip {
         printingThread.detach();
 
         while (true) {
-            sockaddr_in clientAddr;
-            socklen_t clientAddrLen = sizeof(clientAddr);
-            int clientSocket = accept(_serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
-            if (clientSocket < 0)
-                continue;
-            std::string ip = inet_ntoa(clientAddr.sin_addr);
-            int port = ntohs(clientAddr.sin_port);
-
-            std::string clientId = ip + ":" + std::to_string(port);
-
-            std::thread clientThread(&App::handleClient, this, clientSocket, clientAddr);
-            clientThread.detach();
+            try {
+                std::shared_ptr<Socket> clientSocketPtr(new Socket(_serverSocket.accept()));
+                std::thread clientThread(&App::handleClient, this, std::shared_ptr<Socket>(clientSocketPtr));
+                clientThread.detach();
+            } catch (...) {
+            }
         };
     }
 
@@ -85,24 +58,19 @@ namespace flip {
         }
     }
 
-    void App::handleClient(int clientSocket, sockaddr_in clientAddr)
+    void App::handleClient(std::shared_ptr<Socket> clientSocketPtr)
     {
-        std::string ip = inet_ntoa(clientAddr.sin_addr);
-        std::string clientId = ip + ":" + std::to_string(ntohs(clientAddr.sin_port));
-        std::cout << "Connection" << clientId << std::endl;
-        char buffer[4096];
-        int bytesRead;
+        Socket &clientSocket = *clientSocketPtr;
+        std::string clientID = clientSocket.getID();
 
-        while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
-            std::unique_lock<std::mutex> lock(_mutex);
-            _clientDataMap[clientId].fifo.push(std::string(buffer, bytesRead));
-            lock.unlock();
-        }
+        std::cout << "Connection: " << clientID << std::endl;
+
+        std::string buffer = clientSocket.receive();
+        std::unique_lock<std::mutex> lock(_mutex);
+        _clientDataMap[clientID].fifo.push(buffer);
+        lock.unlock();
         sem_post(&requests);
 
-        if (!bytesRead)
-            std::cout << clientId << "disconnected" << std::endl;
-
-        close(clientSocket);
+        std::cout << clientID << " disconnected" << std::endl;
     }
 }
--- sources/Socket.cpp
+++ sources/Socket.cpp
@@ -69,8 +69,10 @@ namespace flip {
     std::string Socket::receive(void) const
     {
         char buffer[BUFFER_SIZE];
-        ssize_t nb_bytes = recv(_fd, buffer, BUFFER_SIZE - 1, 0);
+        ssize_t nb_bytes = 0;
 
+        for (int nb = recv(_fd, buffer, BUFFER_SIZE - 1, 0); nb == BUFFER_SIZE - 1; nb_bytes += nb)
+            nb = recv(_fd, buffer, BUFFER_SIZE - 1, 0);
         if (nb_bytes == -1)
             throw;
         return std::string(buffer, nb_bytes);
